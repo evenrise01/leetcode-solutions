6efb601fba0ff5b4a34889e9f67262633f1911c7
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2024-12-26 23:48:10 +0530

    Leetcode solutions

---
+++ 2239. Find Closest Number to Zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int closest = nums[0];
+
+        for(int x: nums){
+            if(std::abs(x) < std::abs(closest)){
+                closest = x;
+            }
+        }
+
+        if(closest < 0 && std::find(nums.begin(), nums.end(), std::abs(closest)) != nums.end()){
+            return std::abs(closest);
+        }
+        else{
+            return closest;
+        }
+    }
+};
