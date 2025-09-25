a8a80a2db958c487b2f3eddc3c6f5dac77233564
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-09-25 23:06:48 +0530

    Create 392. Is Subsequence.js

---
+++ 392. Is Subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let sp = 0, tp = 0;
+
+    while (sp < s.length && tp < t.length){
+        if (s[sp] == t[tp]){
+            sp++;
+        }
+        tp++;
+    }
+
+    return sp === s.length
+};
